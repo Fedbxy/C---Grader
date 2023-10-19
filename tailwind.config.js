26cd2af2e9a7087124aa17af3af115a11520a9ff
Fed <punnaphop.sitthi@gmail.com>
2023-10-19 23:22:06 +0700

    include private content

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,6 +1,9 @@
 /** @type {import('tailwindcss').Config} */
 module.exports = {
-  content: ['./public/**/*.{html,js}'],
+  content: [
+    './public/**/*.{html,js}', 
+    './private/**/*.{html,js}',
+  ],
   theme: {
     extend: {},
   },
